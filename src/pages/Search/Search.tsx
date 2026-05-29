eeab38f9382ca8c476e016b8509cfc752c9ac8c4
Gor7yo <gor22.11@bk.com>
2026-05-29 21:02:27 +0300

    add search & fixes

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -13,19 +13,19 @@ export const Search = (): JSX.Element => {
 
   useEffect(() => {
     if (artists.length == 0) {
-      setIsAvalible((state) => (state = true));
+      setIsAvalible((state) => state = true);
     }
     if (artists.length > 0) {
-      setIsAvalible((state) => (state = false));
+      setIsAvalible((state) => state = false);
     }
   }, [artists]);
 
   useEffect(() => {
     if (artists.length == 0) {
-      setIsAvalible((state) => (state = true));
+      setIsAvalible((state) => state = true);
     }
     if (artists.length > 0) {
-      setIsAvalible((state) => (state = false));
+      setIsAvalible((state) => state = false);
     }
   }, []);
 
